9d010a1dd0da5746eba2dff68ea7aca089d8ba39
Flyr1Q <esimovmiras@gmail.com>
2016-03-29 18:45:30 +0600

    #1 Peak finding - 1d & 2d array

---
+++ peak2d/benchmark.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var Benchmark = require('benchmark');
+var Peak2D = require('./peak2d.js')
+
+var suite = new Benchmark.Suite;
+
+var dataset = [
+  [1, 1, 1, 1, 1],
+  [2, 2, 2, 2, 2],
+  [3, 3, 3, 3, 3],
+  [4, 4, 4, 4, 4],
+  [5, 6, 7, 8, 7]
+];
+
+suite
+  .add('Peak2D#naive', function() {
+    Peak2D.naive(dataset);
+  })
+  .add('Peak2D#halfBinary', function() {
+    Peak2D.halfBinary(dataset);
+  })
+  .on('cycle', function(event) {
+    console.log(String(event.target) + ' ' + event.target.stats.mean);
+  })
+  .run({ 'async': true });
